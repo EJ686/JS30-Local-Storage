92750f1afcb3cfee0fdb502d79de463d0281c2b9
Edgar Juarez <ejjt.686@gmail.com>
2017-06-15 17:42:23 -0700

    Added e listener to add item, clear the input form after submitting and prevent default page refresh'

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem (e) {
+    e.preventDefault(); // Prevents the form from submitting and refreshing page
+    const text = (this.querySelector('[name = item]')).value;
+    const item = {
+        text: text,
+        done: false
+    }
+    this.reset(); // 'This' is the form element and it clears the form field
+}
+
+addItems.addEventListener('submit', addItem);
