153d0864199d8d8e44855b29f4b11b017ddf5078
Edgar Juarez <ejjt.686@gmail.com>
2017-06-15 17:50:46 -0700

    Items now show on the UI in a list

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ function addItem (e) {
         done: false
     }
     items.push(item);
+    populateList(items, itemsList);
     this.reset(); // 'This' is the form element and it clears the form field
-}
+};
+
+function populateList (plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+            <label for="">${plate.text}</label>
+        </li>
+        `;
+    }).join('');
+};
 
 addItems.addEventListener('submit', addItem);
